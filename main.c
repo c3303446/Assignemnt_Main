8c922ba3819139375dc129a4fa7e2952edd83f2a
Isaac Fleury <isaac.fleury@uon.edu.au>
2019-04-16 03:24:10 +0000

    check

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+
+int fileOpen();
+
 int main() {
-	printf("Enter another integer: ");
+    
+    fileOpen();
+    
 	return 0;
+	
+}
+
+int fileOpen(){
+    
+    FILE *input;
+    
+    input = fopen("input.txt", "r");
+    
+    if(input == NULL){
+        printf("the file was not opened");
+    }
+    else{
+        printf("the file was opened");
+    }
+    
+    return(0);
 }
