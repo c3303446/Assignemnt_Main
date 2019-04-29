286d824838c6add8b09839862bd3e08a6502aa7c
Isaac Fleury <isaac.fleury@uon.edu.au>
2019-04-29 05:58:51 +0000

    SAVE

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+iun 
+
+
+
 #include <stdio.h> //inlcuding libraries that will be used within the programe
 
 //include function for each question on project notes where user inputs in a menu system in the main function
@@ -38,247 +42,244 @@ int main() {        //beginning of main function
     
     printf("Selection: ");
     
-    scanf("%d", &choice); //receives input from user as per function choice
+    scanf("%d", &choice);   //receives input from user as per function choice
     
-    switch(choice){       // start switch function for user choice
+    switch(choice){         // start switch function for user choice
         
-        case 1:           // if choice is = 1
+        case 1:             // if choice is = 1
         
-            taskOne();    // 
+            taskOne();      // call function taskOne()
         
-            break;
+            break;          // end program after taskOne has completed
         
-        case 2:         
+        case 2:             // if choice is = 2
         
-            taskTwo();
+            taskTwo();      // call function taskTwo()
             
-            break;
+            break;          // end program after taskTwo has completed
         
-        case 3:
+        case 3:             // is choice is = 3
             
-            taskThree();
+            taskThree();    // call function taskThree()
             
-            break;
+            break;          // end program after taskThree has completed  
         
-        case 4:
+        case 4:             // if choice is = 4
             
-            taskFour();
+            taskFour();     // call function taskFour()
             
-            break;
+            break;          // end program after taskFour has completed
         
-        default:
+        default:            // if another choice value has been inputed then printf
         
             printf("Your choice does not match any of the listed tasks!\n");
             
         }
     
-        return 0;
+        return 0;           // return 0 to main function (nothing happens)
     }
     
 
-int taskOne(){
+int taskOne(){              // function taskOne
     
-    FILE *input;
+    FILE *input;            // declare file variable
     
-    int key;
+    int key;                // delcare variable of key value
     
-    int i = 0;
+    int i = 0;              // declare count variable
     
-    char inp[100];
+    char inp[100];          // declare input array and size
     
-    char hold;
+    char hold;              // declare hold variable for loop
     
     printf("Please enter the key you would like to use: ");
     
-    scanf("%d", &key);
+    scanf("%d", &key);      // get user input and assign to variable 'key'
 
-    input = fopen("input.txt", "r");
+    input = fopen("input.txt", "r");        // open input file from given directory for reading 
      
-     while(!feof(input)){
+     while(!feof(input)){                   // begin while loop while does not = EOF
          
-         fscanf(input, "%c", &inp[i]);
+         fscanf(input, "%c", &inp[i]);      // file scan positon of character in the 'i' position
          
-         if(inp[i] == 32 ){
+         if(inp[i] == 32 ){                 // if the character = ' ' then print and end loop for character position
              
             printf("%c", inp[i]);
             
-            i++;
+            i++;                            // loop increment for i + 1
          
          } 
-         else {
+         else {                                         // if inp[i] != 32 then...
              
-            if(inp[i] >= 'A' && inp[i] <= 'Z'){
+            if(inp[i] >= 'A' && inp[i] <= 'Z'){         // ifinp[i] is between and including A and Z then...
                 
-                hold = (inp[i] - key + 65) % 26;
+                hold = (inp[i] - key + 65) % 26;        // implementing formula (m + k)(mod 26) 
                 
-                printf("%c", (hold + 65));
+                printf("%c", (hold + 65));              // print characte the lines with hold + 65
                 
-                i++;
+                i++;                                    // loop increment for i + 1
                 
             }
             
             else {
-                printf("%c", inp[i]);
+                printf("%c", inp[i]);                   // if is outside A and Z then do not change and print character
             }
 
          }
 
      }
-     printf("\n");
-         return 0;
+     printf("\n");                                      // keep output looning nice
+         return 0;                                      // return value set
     }
     
-    int taskTwo(){
+    int taskTwo(){                                      // function taskTwo
         
-        FILE *input;
+        FILE *input;                                    // set file variable
     
-        int key;
+        int key;                                        // declare key variable
     
-        int i = 0;
+        int i = 0;                                      // declare i count variable to 0
     
-        char inp[100];
+        char inp[100];                                  // declare input array and size
     
-        char hold;
+        char hold;                                      // declare hold variable
     
         printf("Please enter the key you would like to use: ");
     
-        scanf("%d", &key);
+        scanf("%d", &key);                              // get user input for key and set to key variable
 
-        input = fopen("input.txt", "r");
+        input = fopen("input.txt", "r");                // open file from given directory for reading
      
-        while(!feof(input)){
+        while(!feof(input)){                            // begin while loop while input does not = EOF
          
-            fscanf(input, "%c", &inp[i]);
+            fscanf(input, "%c", &inp[i]);               // file scan position i as character to array inp[i]
          
-            if(inp[i] == 32 ){
+            if(inp[i] == 32 ){                          // if position i = 32 do not change and print inp[i]
              
                 printf("%c", inp[i]);
             
-                i++;
+                i++;                                    // increment i + 1
          
             } 
-            else {
-             
-            if(inp[i] >= 'A' && inp[i] <= 'Z'){
+            else if(inp[i] >= 'A' && inp[i] <= 'Z'){    // if inp[i] is between and including A and Z then...
                 
-                hold = (inp[i] + key - 65) % 26;
+                    hold = (inp[i] + key - 65) % 26;    // implemented (c - k)(mod 26) and set value to variable hold
                 
-                printf("%c", (hold + 65));
+                    printf("%c", (hold + 65));          // print value of hold and + 65 to bring back to capital values
                 
-                i++;
+                    i++;                                // increment i for next loop
                 
             }
-            
-            else {
+
+            else {                                      // if inp[i] is not between A and Z then do not change adn print character inp[i]
                 
                 printf("%c", inp[i]);
                 
             }
             
          }
-    
-    }
 
-printf("\n");
+printf("\n");                                           // new line to structure output
 
-return 0;
+return 0;                                               // return 0 to function
+
 }
 
-void taskThree(){
+void taskThree(){                                       // function taskThree
     
-    FILE *input;
+    FILE *input;                                        // file variable declaratoin
     
-    int i = 0, x, hold;
+    int i = 0, x, hold;                                 // declare integer variables: count to 0, x, hold 
     
-    char keyArr[100];
+    char keyArr[100];                                   // declare character keyArr array and size
     
-    char inp[100];
+    char inp[100];                                      // declare character inp array and size
     
-    printf("Please enter the sub you would like to use: ");
+    printf("Please enter the sub you would like to use: "); //ask user to enter a alphabet sub
     
-    scanf("%s", keyArr);
+    scanf("%s", keyArr);                                // scan user alphabet sub into keyArr 
 
-    input = fopen("input.txt", "r");
+    input = fopen("input.txt", "r");                    // assign input file to file directory as a read file
     
-    while(!feof(input)){
+    while(!feof(input)){                                // begin while loop: while input  
          
-        fscanf(input, "%c", &inp[i]);
+        fscanf(input, "%c", &inp[i]);                   // file scan from input as a character to inp[i] position
          
-        if(inp[i] == 32 ){
+        if(inp[i] == 32 ){                              // if inp[i] = 32 (space bar) then do no change and print this position
              
             printf("%c", inp[i]);
             
-            i++;
+            i++;                                        // increment i for next loop
             
         }
     
-        else{
+        else{                                           // if it is not then...
         
-                for(x = 0; x < 26; x++){
+                for(x = 0; x < 26; x++){                // start for loop until x is 26 
             
-                    hold = x + 65;
+                    hold = x + 65;                      // make hold = x + 65 to be within the capitals alphabet
             
-                    if(inp[i] == hold){
+                    if(inp[i] == hold){                 // if inp[i] = our value for hold then...
                     
-                        inp[i] = keyArr[x];
+                        inp[i] = keyArr[x];             // make inp[i] = keyArr[x] to set the encrytped sub to our input
                         
-                        printf("%c", inp[i]);
+                        printf("%c", inp[i]);           // print the encrypted character from previous set position inp[i]
                         
-                        break;
+                        break;                          // break for loop 
                         
                     }
                 
                 }
                 
-            i++;
+            i++;                                        // i increment for while loop
         
         }
 
     }
     
-    printf("\n");
-    
-return;
+    printf("\n");                                       // output structure
+        
+return;                                                 // void function return
 
 }
 
-void taskFour(){
+void taskFour(){                                        // function taskFour
     
-    FILE *input;
+    FILE *input;                                        // declare file variable
     
-    int i = 0, x;
+    int i = 0, x;                                       // declare integer i = 0, x
     
-    char keyArr[100];
+    char keyArr[100];                                   // declare character arry key Arr and size
     
-    char inp[100];
+    char inp[100];                                      // delcare character array inp and size
     
-    char alpha[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char alpha[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";      // declare character array alpha and size for comparison to inp[]
     
-    printf("Please enter the sub you would like to use: ");
+    printf("Please enter the sub you would like to use: "); // ask user for keyArr[]
     
-    scanf("%s", keyArr);
+    scanf("%s", keyArr);                                // scan for user input and set to keyArr
 
-    input = fopen("input.txt", "r");
+    input = fopen("input.txt", "r");                    // set variable 'input' to file directory as a read file
     
-    while(!feof(input)){
+    while(!feof(input)){                                // begin while loop while input does not = EOF
          
-        fscanf(input, "%c", &inp[i]);
+        fscanf(input, "%c", &inp[i]);                   // scan file for per position and set as character into position inp[i]
          
-        if(inp[i] == 32 ){
+        if(inp[i] == 32 ){                              // if inp[i] = 32 (space) do not change
              
-            printf("%c", inp[i]);
+            printf("%c", inp[i]);                       // print unchanged inp[i]
             
-            i++;
+            i++;                                        // i incrememnt i + 1
             
         }
     
-        else{
+        else{                                           // if not 32 then... 
         
-                for(x = 0; x < 26; x++){
+                for(x = 0; x < 26; x++){                // start for loop until x = 26
             
-                    if(inp[i] == keyArr[x]){
+                    if(inp[i] == keyArr[x]){            // if inp[i] = keyArr[x] then... if not then keep looping until keyArr[x] is the same as inp[i]
                         
-                        inp[i] = alpha[x];
+                        inp[i] = alpha[x];              // make inp
                         
                         printf("%c", inp[i]);
                         
